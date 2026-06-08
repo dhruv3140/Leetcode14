12c1277f2c15c02f5c6ea7426a1a7be14ebd4188
dhruv3140 <dhruvsinghal166@gmail.com>
2026-06-08 10:47:44 +0530

    Time: 5 ms (73.56%) | Memory: 159.9 MB (61.37%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        int a = 0;
+        int [] ans = new int[nums.length];
         for(int i=0; i<nums.length; i++){
-            if(nums[i]>pivot){
-                list.add(nums[i]);
-            }else if (nums[i]<pivot){
-                list1.add(nums[i]);
-            }else{
-                list2.add(nums[i]);
+            if(nums[i]<pivot){
+                ans[a++] = nums[i];
             }
         }
-        int count = 0;
-        int ans[] = new int[nums.length];
-        for(int i =0; i<list1.size(); i++){
-            ans[i] = list1.get(i);
-            count++;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==pivot){
+                ans[a++] = nums[i];
+            }
         }
-        for(int i =0; i<list2.size(); i++){
-            ans[count] = list2.get(i);
-            count++;
-        }
-        for(int i =0; i<list.size(); i++){
-            ans[count] = list.get(i);
-            count++;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>pivot){
+                ans[a++] = nums[i];
+            }
         }
         return ans;
     }
